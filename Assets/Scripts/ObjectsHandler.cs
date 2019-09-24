c9447254e8e734a51dacc3d0105c6e4a73972647
hankevin0618 <hankevin0618@gmail.com>
2019-09-24 22:52:31 +1000

    Pooping - random position needs to be done

--- Assets/Scripts/ObjectsHandler.cs
+++ Assets/Scripts/ObjectsHandler.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class ObjectsHandler : MonoBehaviour
 {
 
-    public ObjectsHandler objectsHandler;
+    public ObjectsHandler meatHandler;
+    public ObjectsHandler pooHandler;
+
+    // Meats variables
     public int maxMeats = 3;
 
     private int numOfMeats = 0;
     private string meatName = "meat(Clone)";
 
 
+    // Poops variables
+    
+    private int maxPoos = 6;
+    private int numOfPoos = 0;
+    private string pooName = "poo(Clone)";
 
+    private float minXPos = 0f;
+    private float maxXPos = 0f;
+    private float minYPos = 0f;
+    private float maxYPos = 0f;
 
 
     private SpriteRenderer renderer2D;
@@ -45,18 +57,13 @@ public class ObjectsHandler : MonoBehaviour
 
     public void CleanUp() {
 
-        
-        
-
-        
-
-
         if(numOfMeats > 0){
             
             for(int i = 0; i < numOfMeats; i++)
             {
                 
                 DestroyImmediate(GameObject.Find(meatName));
+                DestroyImmediate(GameObject.Find(pooName));
                 //Debug.Log("removed meat");
             }
             
@@ -65,11 +72,7 @@ public class ObjectsHandler : MonoBehaviour
             
         }
 
-        
 
-
-        
-        
         
     }
 
@@ -78,11 +81,32 @@ public class ObjectsHandler : MonoBehaviour
             numOfMeats++;
             var t = transform;
             t.TransformVector(100, 0, 0);
-            var clone = Instantiate(objectsHandler, t.position, Quaternion.identity);
+            var clone = Instantiate(meatHandler, t.position, Quaternion.identity);
             var body2d = clone.GetComponent<Rigidbody2D>();
             body2d.AddForce(Vector3.right * Random.Range(-100, 100));     
         }
         
+    }
+
+    public void Poop() {
+
+        /*
+        Location limitations
+        
+        Transform - Position
+        x: -2.7 ~ 2.7
+        y: -1.689 ~ -4.541
+        
+         */
+        if(numOfPoos < maxPoos){
+            numOfPoos++;
+            Vector3 pooPosition = new Vector3(0, 0, 0);
+            var clone = Instantiate(pooHandler, pooPosition, Quaternion.identity);
+            // 밑에 링크타고 랜덤 적용해서 응가가 나타날 수 있게 하는거부터 시작하렴
+            //https://docs.unity3d.com/ScriptReference/Random.Range.html
+        } else{
+            //something bad thing happens
+        }
     }
 
 }
